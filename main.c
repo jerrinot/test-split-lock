da07b1421b15f2426eef73de3ff7c737a4f146e0
jerrinot <jaromir.hamala@gmail.com>
2020-06-01 16:11:56 +0200

    simplifications

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 #include <time.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 #define CLOCK_TYPE CLOCK_MONOTONIC
 #define CACHE_LINE_SIZE 64u
 
-bool isDeadlineReached(struct timespec* deadline) {
+bool shouldLog(struct timespec* deadline) {
     struct timespec currentTime;
     clock_gettime(CLOCK_TYPE, &currentTime);
     if (currentTime.tv_sec < deadline->tv_sec) {
@@ -20,26 +21,43 @@ bool isDeadlineReached(struct timespec* deadline) {
     return currentTime.tv_nsec >= deadline->tv_nsec;
 }
 
+bool shouldSplit(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Missing Mode. Either 'aligned' or 'split' as a parameter!\n");
+        exit(-1);
+    }
+    char *arg = argv[1];
+    if (strncmp(arg, "aligned", strlen("aligned")) == 0) {
+        return false;
+    } else if (strncmp(arg, "split", strlen("split")) == 0) {
+        return true;
+    } else {
+        printf("Missing Mode. Either 'aligned' or 'split' as a parameter!\n");
+        exit(-1);
+    }
+}
+
 int main(int argc, char *argv[]) {
-    uintptr_t buff = (uintptr_t)malloc(2 * CACHE_LINE_SIZE);
-    printf("Address from malloc: %lu\n", buff);
+    int *ptr = malloc(2 * CACHE_LINE_SIZE);
+    printf("Address from malloc: %p\n", ptr);
 
-    //move the pointer to the last byte in the cache line
-    buff = buff | (CACHE_LINE_SIZE - 1);
-
-    int *ptr = (int *) buff;
-    printf("Address after adjusting: %p\n", ptr);
+    if (shouldSplit(argc, argv)) {
+        //move the pointer to the last byte in the cache line
+        ptr = (int *) (((uintptr_t) ptr) | (CACHE_LINE_SIZE - 1));
+        printf("Address after adjusting: %p\n", ptr);
+    } else {
+        printf("Not lock splitting, relying on a compiler to have the address aligned within a single cache line\n");
+    }
 
     struct timespec next;
     clock_gettime(CLOCK_TYPE, &next);
     next.tv_sec += 1;
 
-    unsigned int counter = 0;
+    uint counter = 0;
     for (;;) {
-        atomic_fetch_add (ptr, 1);
-        if (isDeadlineReached(&next)) {
-            printf("Iteration per second: %i\n", counter);
-            printf("Address: %ls\n", ptr);
+        atomic_fetch_add(ptr, 1);
+        if (shouldLog(&next)) {
+            printf("Iteration per second: %u\n", counter);
             next.tv_sec += 1;
             counter = 0;
         }
